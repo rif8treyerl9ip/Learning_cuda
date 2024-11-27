c5eb94d9d5ba1badedae7bedffbb1307a3625048
rif8treyerl9ip <rif8treyerl9ip@users.noreply.github.com>
2024-11-28 00:40:36 +0900

    refactor: rename file to reflect expanded arithmetic operations

--- operator/scalar_arithmetic.cu
+++ operator/scalar_arithmetic.cu
@@ -5,11 +5,15 @@ __global__ void ScalarAdd(float* a, float* b, float* c)
 {
     *c = *a + *b;
 }
+__global__ void ScalarMultiply(float* a, float* b, float* c)
+{
+    *c = *a * *b;
+}
 
 int main()
 {
     // Host variables
-    float h_a = 1.5f;
+    float h_a = 1.0f;
     float h_b = 2.7f;
     float h_c = 0.0f;
 
@@ -19,20 +23,17 @@ int main()
     cudaMalloc(&d_b, sizeof(float));
     cudaMalloc(&d_c, sizeof(float));
 
-    // Copy input data to device
     cudaMemcpy(d_a, &h_a, sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, &h_b, sizeof(float), cudaMemcpyHostToDevice);
 
-    // Launch kernel
     ScalarAdd<<<1, 1>>>(d_a, d_b, d_c);
-
-    // Copy result back to host
     cudaMemcpy(&h_c, d_c, sizeof(float), cudaMemcpyDeviceToHost);
-
-    // Print result
     printf("%.1f + %.1f = %.1f\n", h_a, h_b, h_c);
 
-    // Free device memory
+    ScalarMultiply<<<1, 1>>>(d_a, d_b, d_c);
+    cudaMemcpy(&h_c, d_c, sizeof(float), cudaMemcpyDeviceToHost);
+    printf("%.1f * %.1f = %.1f\n", h_a, h_b, h_c);
+
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
